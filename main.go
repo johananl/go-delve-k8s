9d767205b0131f78a98cce54fdd5644b98c8a7fb
Johanan Liebermann <jliebermann@microsoft.com>
2023-02-01 12:11:30 +0200

    Fix rune conversion
    Signed-off-by: Johanan Liebermann <jliebermann@microsoft.com>

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func genRandomString(in int) (string, error) {
 	for i := 0; i < 5; i++ {
 		// Generate a random rune in the 97-122 ASCII range (a-z).
 		offset := rand.Intn(26)
-		c := 97 + offset
+		c := rune(97 + offset)
 		out += string(c)
 	}
 
